8955a69c0b409a8ee7c2a1d3f9c36eac6c3cba7d
kraj09 <49805482+kraj09@users.noreply.github.com>
2022-01-01 09:11:42 +0530

    Add project files.

---
+++ SignalRBasics/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRBasics.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
